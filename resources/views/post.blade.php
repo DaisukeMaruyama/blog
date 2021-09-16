af6a0b9d6903859982022cdd5ebac27c8b7fc73b
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-16 15:41:28 +0900

    [Update]部分テンプレート作成

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>My blog</title>
-        <link rel="stylesheet" href="app.css">
-    </head>
-    <body>
-        <article>
-            <div class="top">
-                <h1><a href="/post">Hello world</a></h1>
-                <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
-            </div>
-        </article>
-
-        <a href="/">Back</a>
-    </body>
-</html>
+<x-layout>
+    <div class="top">
+        <h1><a href="/post">Hello world1</a></h1>
+        <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
+    </div>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>My blog</title>
-        <link rel="stylesheet" href="app.css">
-    </head>
-    <body>
-        <div class="top">
-            <h1><a href="/post">Hello world1</a></h1>
-            <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
-        </div>
-        <div class="top">
-            <h1><a href="/post">Hello world2</a></h1>
-            <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
-        </div>
-        <div class="top">
-            <h1><a href="/post">Hello world3</a></h1>
-            <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
-        </div>
-    </body>
-</html>
+<x-layout>
+    <div class="top">
+        <h1><a href="/post">Hello world1</a></h1>
+        <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
+    </div>
+    <div class="top">
+        <h1><a href="/post">Hello world2</a></h1>
+        <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
+    </div>
+    <div class="top">
+        <h1><a href="/post">Hello world3</a></h1>
+        <p>Laravel is incredibly scalable. Thanks to the scaling-friendly nature of PHP and Laravel's built-in support for fast, distributed cache systems like Redis, horizontal scaling with Laravel is a breeze. In fact, Laravel applications have been easily scaled to handle hundreds of millions of requests per month.</p>
+    </div>
+</x-layout>
+
