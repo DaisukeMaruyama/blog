f7638914cbdd163d393607e65d710d40990715d8
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-20 14:05:51 +0900

    [Update]slugの受け渡し

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @foreach ($posts as $post)
         <div class="top">
-            <h1><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></h1>
+            <h1><a href="{{ route('posts.show', $post->slug) }}">{{ $post->title }}</a></h1>
             <h5>{{ $post->excerpt }}</h5>
             <p>{!! nl2br(e($post->body)) !!}</p>
         </div>
