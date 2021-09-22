5e64e55fbefce8f9af2bc81713c699d059358870
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-22 11:30:51 +0900

    [Update]postデータを作成したらそれに付随して他のモデルのダミーデータを作成できるようにする

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,58 +21,61 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-        $user = User::create([
-            'username' => '管理者ABC',
-            'email' => 'abc@example.com',
-            'password' => bcrypt('test1234'),
-        ]);
+        Post::factory(5)->create();
+        //$user = User::factory()->create();
 
-        $user2 = User::create([
-            'username' => '管理者DEF',
-            'email' => 'def@example.com',
-            'password' => bcrypt('test5678'),
-        ]);
+        //$user = User::create([
+        //    'username' => '管理者ABC',
+        //    'email' => 'abc@example.com',
+        //    'password' => bcrypt('test1234'),
+        //]);
+//
+        //$user2 = User::create([
+        //    'username' => '管理者DEF',
+        //    'email' => 'def@example.com',
+        //    'password' => bcrypt('test5678'),
+        //]);
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => "My personal Post",
-            'slug' => 'personal',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
-        ]);
-
-        Post::create([
-            'user_id' => $user2->id,
-            'category_id' => $family->id,
-            'title' => "My family Post",
-            'slug' => 'family',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => "My work Post",
-            'slug' => 'work',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
-        ]);
+        //$personal = Category::create([
+        //    'name' => 'Personal',
+        //    'slug' => 'personal',
+        //]);
+//
+        //$family = Category::create([
+        //    'name' => 'Family',
+        //    'slug' => 'family',
+        //]);
+//
+        //$work = Category::create([
+        //    'name' => 'Work',
+        //    'slug' => 'work',
+        //]);
+//
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $personal->id,
+        //    'title' => "My personal Post",
+        //    'slug' => 'personal',
+        //    'excerpt' => 'Lorem ipsum dolar sit amet.',
+        //    'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
+        //]);
+//
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $family->id,
+        //    'title' => "My family Post",
+        //    'slug' => 'family',
+        //    'excerpt' => 'Lorem ipsum dolar sit amet.',
+        //    'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
+        //]);
+//
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $work->id,
+        //    'title' => "My work Post",
+        //    'slug' => 'work',
+        //    'excerpt' => 'Lorem ipsum dolar sit amet.',
+        //    'body' => "We like to call Laravel a progressive framework. By that, we mean that Laravel grows with you. If you're just taking your first steps into web development, Laravel's vast library of documentation, guides, and video tutorials will help you learn the ropes without becoming overwhelmed.",
+        //]);
     }
 }
