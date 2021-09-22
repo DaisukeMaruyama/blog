29e2c34f8f68a9424928b5539c0fdfe08d1fb86b
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-22 14:22:04 +0900

    [Update]backリンクをtopに含める

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,6 @@
         <h1>{{ $post->title }}</h1>
         <p> By <a href="#">{{ $post->user->username }}</a> in category: {{ $post->category->name }}</p>
         <p>{{ $post->body }}</p>
+        <a href="/">Back</a>
     </div>
-    <a href="/">Back</a>
 </x-layout>
