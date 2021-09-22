7c5d86fda41977ef3b43f2ad8f1674bc9e82c5ec
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-22 14:20:49 +0900

    [Update]authorをuserに戻す

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>My blog</title>
-        <link rel="stylesheet" href="app.css">
+        <link rel="stylesheet" href="{{ asset('app.css') }}">
     </head>
     <body>
         {{ $slot }}
