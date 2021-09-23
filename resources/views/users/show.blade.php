2f710b76553caa44049694afc9a9e3697076bc8c
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-23 10:23:28 +0900

    [Update]userに紐ついたpostsを表示

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,6 +2,9 @@
     <div class="top">
         <h3>Name: {{ $user->username }}</h3>
         <p>Books wriiten by {{ $user->username }}</p>
+        @foreach ($user->posts as $post)
+            <p>title: {{ $post->title }} </p>
+        @endforeach
         {{--
         <ul>
             @foreach ($userBook as $book)
