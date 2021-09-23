401453b44d921469ae817a592306de4787cb1506
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-23 10:31:12 +0900

    [Update]userに紐ついたpostsの変数を表示

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,15 +2,15 @@
     <div class="top">
         <h3>Name: {{ $user->username }}</h3>
         <p>Books wriiten by {{ $user->username }}</p>
+        {{--
         @foreach ($user->posts as $post)
             <p>title: {{ $post->title }} </p>
         @endforeach
-        {{--
+        --}}
         <ul>
             @foreach ($userBook as $book)
-                <li>{{ $book }}</li>
+                <li>Title: {{ $book->title }}</li>
             @endforeach
         </ul>
-        --}}
     </div>
 </x-layout>
