4f6322287647bce10e8dcd177d86ba0bc9d00337
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-23 10:20:26 +0900

    [Update]user詳細画面への遷移リンク作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="top">
         <h1>{{ $post->title }}</h1>
-        <p> By <a href="#">{{ $post->user->username }}</a> in category: {{ $post->category->name }}</p>
+        <p> By <a href="{{ route('users.show', $post->user->id) }}">{{ $post->user->username }}</a> in category: {{ $post->category->name }}</p>
         <p>{{ $post->body }}</p>
         <a href="/">Back</a>
     </div>
