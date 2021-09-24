36153f8dc7d91966716d1136dc8ec7bfd8955214
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-24 09:52:38 +0900

    [Update]categoryのshowページへ遷移させる

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="top">
         <h1>{{ $post->title }}</h1>
-        <p> By <a href="{{ route('users.show', $post->user->id) }}">{{ $post->user->username }}</a> in category: {{ $post->category->name }}</p>
+        <p> By <a href="{{ route('users.show', $post->user->id) }}">{{ $post->user->username }}</a> in category: <a href="{{ route('categories.show', $post->category->slug) }}">{{ $post->category->name }}</a></p>
         <p>{{ $post->body }}</p>
         <a href="/">Back</a>
     </div>
