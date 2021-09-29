7538324dc3450cbd1010e03d54ad1298912a2ddc
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-29 11:01:06 +0900

    [Update]selectのoptionにcategoryを表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 class PostController extends Controller
 {
     //withでデータとることでデータ通信を早くする
     public function index() {
         $posts = Post::with('category')->latest()->get();
+        $categories = Category::all();
 
         return view('posts.index')
-          ->with(['posts' => $posts]);
+          ->with([
+              'posts' => $posts,
+              'categories' => $categories,
+          ]);
     }
 
     //implicit bindingでかく
