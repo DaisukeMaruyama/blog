20f513dcb4e396e2c86948ec3a48ca86f8b861d0
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-29 10:17:40 +0900

    [Update]インデントを整える

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,6 @@
         {{-- components/post-featured-card.blade.phpで表示--}}
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
-
         @else
             <p class="text-center">No posts yet. Please check back later</p>
         @endif
