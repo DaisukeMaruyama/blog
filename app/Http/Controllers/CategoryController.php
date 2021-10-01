11ba2857bfa7170d3ef6b7cc208dfa80de9c1449
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-01 17:30:59 +0900

    [Update]カテゴリー検索機能

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Post;
 class CategoryController extends Controller
 {
 
     public function show($slug)
     {
-        $categories = Category::where('slug', $slug)->get();
+        $category = Category::where('slug', $slug)->first();
 
+        $id = $category->id;
+        $posts = Post::where('id', $id)->get();
         return view('categories.show')
-          ->with(['categories' => $categories]);
+          ->with(['category' => $category],['posts' => $posts]);
     }
 
 }
