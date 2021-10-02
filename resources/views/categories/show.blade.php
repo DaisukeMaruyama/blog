6a1edfb630309784d84de99e1706190dc53d2d56
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-02 13:23:25 +0900

    [Update]検索結果を表示させる

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     {{-- view/_post-header.blade.php部分テンプレートで表示--}}
-
+    @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
             {{-- components/post-featured-card.blade.phpで表示--}}
