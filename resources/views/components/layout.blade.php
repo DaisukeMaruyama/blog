b01d2206d95718841d40b1f5b94ea258b3aa6767
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-07 10:09:01 +0900

    [Update]alpine.jsでsetTimeout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -71,7 +71,10 @@
         </footer>
     </section>
     @if (session()->has('success'))
-        <div class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl right-3 bottom-3 text-sm">
+        <div x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 4000)"
+            x-show="show"
+            class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl right-3 bottom-3 text-sm">
             <p>{{ session('success') }}</p>
         </div>
     @endif
