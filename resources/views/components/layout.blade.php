ed43a87b155d57c955088f293688876db1e753fe
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-07 10:13:22 +0900

    [Update]successmessageを部分テンプレート化

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -70,13 +70,8 @@
             </div>
         </footer>
     </section>
-    @if (session()->has('success'))
-        <div x-data="{ show: true }"
-            x-init="setTimeout(() => show = false, 4000)"
-            x-show="show"
-            class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl right-3 bottom-3 text-sm">
-            <p>{{ session('success') }}</p>
-        </div>
-    @endif
+
+    <x-flash />
+
 </body>
 
