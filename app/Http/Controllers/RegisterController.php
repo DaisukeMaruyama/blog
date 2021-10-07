f8f5783d9cca1786591b13d69760bdf8803adcc4
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-07 09:16:45 +0900

    [Update]ユーザ登録controller作成

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class RegisterController extends Controller
 {
     public function create()
     {
         return view('register.create');
 
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'username' => 'required | min:3 | max:255',
+            'email'=> 'required | email | max:255',
+            'password' => 'required | min:6 | max:255',
+        ]);
+
+        // passwordを暗号化
+        $attributes['password'] = bcrypt($attributes['password']);
+
+        User::create($attributes);
+
+        return redirect('/');
     }
 }
