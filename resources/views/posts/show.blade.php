17d16ab8b4dc6eec0a3edbcc74aa2506733d2c37
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-10 14:12:01 +0900

    [Update]レイアウト調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,10 +52,10 @@
                 <section class="col-span-8 col-start-5 mt-10">
                     <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
                         <div class="flex-shrink-0">
-                            <img src="http://i.pravatar.cc/60" alt="" width="60" height="60">
+                            <img src="http://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
                         </div>
                         <div>
-                            <header>
+                            <header class="mb-4">
                                 <h3 class="font-bold">John Doe</h3>
                                 <p class="text-sm">Posted
                                     <time>8 months ago</time>
