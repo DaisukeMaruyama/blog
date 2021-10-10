cdfcf24f5abe28218b7177d6b56a836d91b2097a
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-10 13:51:35 +0900

    [Update]commentビュー作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,6 +50,26 @@
                     </div>
                 </div>
             </article>
+
+            <section>
+                <article class="flex">
+                    <div>
+                        <img src="http://i.pravatar.cc/100" alt="">
+                    </div>
+                    <div>
+                        <header>
+                            <h3 class="font-bold">John Doe</h3>
+                            <p class="text-sm">Posted
+                                <time>8 months ago</time>
+                            </p>
+                        </header>
+                        <p>
+                            Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
+                        </p>
+                    </div>
+                </article>
+            </section>
+
         </main>
     </section>
 
