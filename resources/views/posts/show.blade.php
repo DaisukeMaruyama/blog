01a88d7949c747d4ce1d40de9a0e01f4db5b48ff
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-10 14:10:51 +0900

    [Update]レイアウト調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,26 +49,25 @@
                         {{ $post->body }}
                     </div>
                 </div>
-            </article>
-
-            <section class="col-span-8 col-start-5 mt-10">
-                <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-                    <div class="flex-shrink-0">
-                        <img src="http://i.pravatar.cc/60" alt="" width="60" height="60">
-                    </div>
-                    <div>
-                        <header>
-                            <h3 class="font-bold">John Doe</h3>
-                            <p class="text-sm">Posted
-                                <time>8 months ago</time>
+                <section class="col-span-8 col-start-5 mt-10">
+                    <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+                        <div class="flex-shrink-0">
+                            <img src="http://i.pravatar.cc/60" alt="" width="60" height="60">
+                        </div>
+                        <div>
+                            <header>
+                                <h3 class="font-bold">John Doe</h3>
+                                <p class="text-sm">Posted
+                                    <time>8 months ago</time>
+                                </p>
+                            </header>
+                            <p>
+                                Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
                             </p>
-                        </header>
-                        <p>
-                            Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
-                        </p>
-                    </div>
-                </article>
-            </section>
+                        </div>
+                    </article>
+                </section>
+            </article>
 
         </main>
     </section>
