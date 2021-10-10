b0ea0531eda3d7202cf03a8a76fd2e300df0ef3d
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-10 14:14:08 +0900

    [Update]部分テンプレート化

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,23 +50,9 @@
                     </div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10">
-                    <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-                        <div class="flex-shrink-0">
-                            <img src="http://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
-                        </div>
-                        <div>
-                            <header class="mb-4">
-                                <h3 class="font-bold">John Doe</h3>
-                                <p class="text-sm">Posted
-                                    <time>8 months ago</time>
-                                </p>
-                            </header>
-                            <p>
-                                Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
-                            </p>
-                        </div>
-                    </article>
-                </section>
+
+                    <x-post-comment />
+                    
             </article>
 
         </main>
