15b2878ac9bfe5c8749497b28e376e95f12d227d
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-10 15:48:16 +0900

    [Update]関連付いたコメントを表示

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,18 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
     <div class="flex-shrink-0">
-        <img src="http://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="http://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->user->username }}</h3>
             <p class="text-sm">Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
         <p>
-            Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
