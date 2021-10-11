e514824a02f1b1f61ac3f702962d8c428bb8d42a
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-11 11:03:24 +0900

    [Update]formレイアウト編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -54,11 +54,18 @@
                     <form method="post" action="#" class="border boder-gray-200 p-6 rounded-xl">
                         @csrf
 
-                        <header class="flex">
-                            <img src="http://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-xl">
-                            <h2>Want to participate?</h2>
+                        <header class="flex text-center">
+                            <img src="http://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                            <h2 class="ml-4">Want to participate?</h2>
                         </header>
 
+                        <div class="mt-6">
+                            <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Quick, things of something to say!"></textarea>
+                        </div>
+
+                        <div class="flex justify-end mt-10 border-t border-gray-200 pt-6">
+                            <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                        </div>
 
                     </form>
 
