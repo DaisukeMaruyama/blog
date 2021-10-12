6600250a41fca3af3e18ee442192406d0adac50e
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-10-12 09:36:22 +0900

    [Update]コメント保存アクション作成

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -9,6 +9,16 @@ class PostCommentsController extends Controller
 {
     public function store(Post $post)
     {
-        
+
+        request()->validate([
+            'body' => 'required'
+        ]);
+
+        $post->comments()->create([
+            'user_id' => request()->user()->id,
+            'body' => request('body'),
+        ]);
+
+        return back();
     }
 }
